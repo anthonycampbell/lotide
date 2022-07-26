28324804e032c1f9ec022578d65c4b26921e6eb2
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-26 14:54:06 -0700

    Made eqArray to compare if 2 arrays are equal for one level deep.

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a1, a2){
+  let l = a2.length;
+  if (a1.length > a2.length) l = a1.length; 
+  for (let i = 0; i < l; i++){
+    if (a1[i] !== a2[i]) return false;
+  }
+  return true
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2], [1, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 1], [1, 2]), false);
