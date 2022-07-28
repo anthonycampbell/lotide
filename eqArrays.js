830d026e5b9e3d9089f953f8ab9ace56fd9d54e9
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-28 13:55:05 -0700

    implemented eqArrays recursively for nesting

--- eqArrays.js
+++ eqArrays.js
@@ -7,10 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(a1, a2) {
-  let l = a2.length;
-  if (a1.length > a2.length) l = a1.length;
+  let l = a1.length;
+  if (l !== a2.length) return false;
   for (let i = 0; i < l; i++) {
-    if (a1[i] !== a2[i]) return false;
+    if (Array.isArray(a1[i]) && Array.isArray(a2[i])){
+      if (!eqArrays(a1[i], a2[i])) return false
+    } else if (a1[i] !== a2[i]) return false;
   }
   return true;
 };
@@ -18,3 +20,6 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays([1, 2], [1, 2, 1]), false);
 assertEqual(eqArrays([1, 2, 1], [1, 2]), false);
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
