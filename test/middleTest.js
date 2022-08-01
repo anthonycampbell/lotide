e0d32e85cc064f1825111d73d76bf2938ccc1423
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-01 03:19:08 -0700

    refactored middle and tail to use mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+  it("middle([1]) is []", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("middle([1,2]) is []", () => {
+    assert.deepEqual(middle([1,2]), []); 
+  });
+  it("middle([1,2,3]) is [2]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]); 
+  });
+  it("middle([1,2,3,4,5]) is [3]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]); 
+  });
+  it("middle([1,2,3,4]) is [2,3]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  });
+  it("middle([1,2,3,4,5,6]) is [3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns 2 for length of tail on ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("returns 'Lighthouse for tail[0] on ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse"); 
+  });
+  it("returns 'labs' for tail[1] on ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs"); 
+  });
+  it("does not return 3 for length of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
+    assert.isFalse(tail(["Yo Yo", "Lighthouse", "Labs"]).length === 3); 
+  });
+});
