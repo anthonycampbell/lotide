e0e6ea9543eecd87a8f01225b08194953eeceb50
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-01 01:15:26 -0700

    moved tail tests to tailTest file

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = require('./assertEqual');
 const tail = function(a) {
   return a.slice(1);
 };
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
